a16e97a0e6c55042a28eb1263f3eafa094d68a79
JavierMarSan18 <marroquinjavier75@gmail.com>
2022-05-17 01:46:58 -0600

    Primer commit api

---
+++ API/polarcity/src/test/java/edu/programacion3/polarcity/PolarcityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.programacion3.polarcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarcityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
