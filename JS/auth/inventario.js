d796cdec0c4bb62a7f75e07d55d499680ed8b001
Kevinisky27 <keare27@gmail.com>
2022-06-11 06:52:43 -0600

    Cambios de conexión fallidos

--- JS/auth/inventario.js
+++ JS/auth/inventario.js
@@ -3,22 +3,24 @@ const url = 'https://polarcity.herokuapp.com/api/products'
 function obtenetLocalStorage () {
   if (localStorage.getItem('token')) {
     const token = localStorage.getItem('token')
-
-    fetch(url, {
-      method: "GET",
-        headers: {
-          "Authorization": `${token}`,
-          "Accept": "application/json",
-          "Content-type": "application/json; charset=UTF-8"},
-        body: JSON.stringify(login)
-    })
-    .then(res =>  res.json())
-    .catch(err => console.log(err))
+    traerDatos(token)
   } 
   else {
     console.log("Token no existente en el local Storage")
   }
 }
 
+function traerDatos (token) {
+  fetch(url,{
+    method: 'GET',
+    headers: { 
+      "Authorization": `Bearer ${token}`,
+      "Accept": "application/json",
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then((res) => res.json())
+  .catch(err => console.log(err))
+}
 obtenetLocalStorage()
 
